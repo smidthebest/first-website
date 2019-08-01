22281b9452b25e1047b3ef1adc0d4d5edd87f724
smidthebest <37987345+smidthebest@users.noreply.github.com>
2019-08-01 14:28:36 -0700

    Add files via upload
    Have made the navigation work and added a singup page which creates new entries to the mysql and the main page is a sign in that checks if the password is correct.

--- demo_mysql.js
+++ demo_mysql.js
@@ -12,6 +12,10 @@ app.get('/index.html', function(req, res) {
     res.sendFile(__dirname + "/index.html"); 
 })
 
+app.get('/signup.html', function(req, res) {
+    res.sendFile("/Users/siddharthamishra/Documents/VSCode/signup.html"); 
+})
+
 var con = mysql.createPool({
     host: "localhost", 
     user: "root",
@@ -19,7 +23,35 @@ var con = mysql.createPool({
     database: "mydb"
 });
 
-var data = [];
+app.get('/process_signup', function(req, res){
+    var email = req.query.first_name; 
+    var pass = req.query.last_name; 
+
+    response = "('" + email + "', '" + pass +"')";
+
+    con.getConnection(function(err){
+        if(err) throw err; 
+        var sql = "INSERT INTO users (email, password) VALUES " + response;
+        con.query("SELECT * FROM users WHERE email = '" + email +"'", function(err, result) {
+            if(err) throw err; 
+            if(result.length == 0){
+                con.query(sql, function (err, result) {
+                    if (err) throw err;
+                });
+                res.redirect("index.html"); 
+            }
+            else {
+                console.log(result); 
+                console.log("Username already exists. Please try a new one.");
+            }
+        })
+
+    })
+
+   
+
+})
+
 app.get('/process_get', function (req, res) {
    // Prepare output in JSON format
    var email = req.query.first_name; 
@@ -33,19 +65,26 @@ app.get('/process_get', function (req, res) {
     var sql = "INSERT INTO users (email, password) VALUES " + response;
     con.query("SELECT * FROM users WHERE email = '" + email +"'", function(err, result) {
         if(err) throw err; 
-        if(result.length == 0) check = false; 
-        
-        if(!check){
-            con.query(sql, function (err, result) {
-                if (err) throw err;
-                
-            });
+        if(result.length != 0) {
+            console.log(result[0].password + " " + pass); 
+            if(result[0].password != pass){
+                console.log("You inputed the wrong password."); 
+            }
+            else {
+                res.redirect("index.html");
+                /*
+                This has to be moved!!
+                */
+            }
+        }
+        else{
+            console.log("You have not created an account"); 
         }
     });
     
   }); 
 
-  res.redirect("index.html");
+  
  
 })
 
