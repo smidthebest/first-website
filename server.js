cf0a954ff6ad2d6dd34f999c8a310ddfd1445c86
Siddhartha Mishra <siddhartham04@gmail.com>
2019-08-13 12:40:24 -0700

    Formated better

--- server.js
+++ server.js
@@ -1,225 +1,221 @@
 var mysql = require("mysql");
 var express = require('express');
 var app = express();
-var path = require('path'); 
-app.set("view engine", "pug"); 
-app.set("first-website", path.join(__dirname, "first-website")); 
+var path = require('path');
+app.set("view engine", "pug");
+app.set("first-website", path.join(__dirname, "first-website"));
 var fs = require('fs');
 
 
 
-var finEmail = ""; 
-var users = {}; 
-app.get('/require.js', function(req, res){
-    res.sendfile(__dirname + "/require.js"); 
+var finEmail = "";
+var users = {};
+app.get('/require.js', function(req, res) {
+	res.sendfile(__dirname + "/require.js");
 })
 app.use(express.static('public'));
-app.get('/login.html', function (req, res) {
-   res.sendFile( __dirname + "/" + "login.html" );
+app.get('/login.html', function(req, res) {
+	res.sendFile(__dirname + "/" + "login.html");
 })
 
-app.use(express.static(path.join(__dirname, 'public2'))); 
+app.use(express.static(path.join(__dirname, 'public2')));
 app.get('/chat.html', function(req, res) {
-    if(finEmail == "") res.redirect("login.html");
-    else res.render('chat.ejs');
+	if (finEmail == "") res.redirect("login.html");
+	else res.render('chat.ejs');
 })
 
 app.get('/signup.html', function(req, res) {
-    res.sendFile(__dirname + "/signup.html"); 
+	res.sendFile(__dirname + "/signup.html");
 })
 
 var pool = mysql.createPool({
-    connectionLimit: 10, 
-    host: "localhost", 
-    user: "root",
-    password: "Firstserver1",
-    database: "mydb"
+	connectionLimit: 10,
+	host: "localhost",
+	user: "root",
+	password: "Firstserver1",
+	database: "mydb"
 });
 
 
-app.get('/process_signup', function(req, res){
-    var email = req.query.first_name; 
-    var pass = req.query.last_name; 
+app.get('/process_signup', function(req, res) {
+	var email = req.query.first_name;
+	var pass = req.query.last_name;
 
-    response = "('" + email + "', '" + pass +"')";
+	response = "('" + email + "', '" + pass + "')";
 
-    pool.getConnection(function(err, con){
-        if(err) throw err; 
-        var sql = "INSERT INTO users (email, password) VALUES " + response;
-        con.query("SELECT * FROM users WHERE email = '" + email +"'", function(err, result) {
-            if(err) throw err; 
-            if(result.length == 0){
-                con.query(sql, function (err, result) {
-                    if (err) throw err;
-                });
-                finEmail = email; 
-                con.query("CREATE TABLE mydb." +finEmail +" (num int, name varchar(255), data longtext, dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)", function(err, result) {
-                    if(err) throw err; 
+	pool.getConnection(function(err, con) {
+		if (err) throw err;
+		var sql = "INSERT INTO users (email, password) VALUES " + response;
+		con.query("SELECT * FROM users WHERE email = '" + email + "'", function(err, result) {
+			if (err) throw err;
+			if (result.length == 0) {
+				con.query(sql, function(err, result) {
+					if (err) throw err;
+				});
+				finEmail = email;
+				con.query("CREATE TABLE mydb." + finEmail + " (num int, name varchar(255), data longtext, dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)", function(err, result) {
+					if (err) throw err;
 
-                }) ; 
-                
-                res.redirect("chat.html"); 
-            }
-            else {
-                
-                console.log("Username already exists. Please try a new one.");
-            }
-        })
-        con.release(); 
+				});
 
-    })
+				res.redirect("chat.html");
+			} else {
 
-   
+				console.log("Username already exists. Please try a new one.");
+			}
+		})
+		con.release();
+
+	})
+
+
 
 })
 
-app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-   var email = req.query.first_name; 
-   var pass = req.query.last_name; 
+app.get('/process_get', function(req, res) {
+	// Prepare output in JSON format
+	var email = req.query.first_name;
+	var pass = req.query.last_name;
 
-   response = "('" + req.query.first_name + "', '" + req.query.last_name +"')";
-   
-   var check = true;  
-   pool.getConnection(function(err, con) {
-    if (err) throw err;
-    var sql = "INSERT INTO users (email, password) VALUES " + response;
-    con.query("SELECT * FROM users WHERE email = '" + email +"'", function(err, result) {
-        if(err) throw err; 
-        if(result.length != 0) {
-            
-            if(result[0].password != pass){
-                console.log("You inputed the wrong password."); 
-            }
-            else {
-                finEmail = email; 
-                res.redirect("chat.html"); 
-                /*
-                This has to be moved!!
-                */
-            }
-        }
-        else{
-            console.log("You have not created an account"); 
-        }
-    });
-    con.release(); 
-  }); 
+	response = "('" + req.query.first_name + "', '" + req.query.last_name + "')";
 
-  
- 
+	var check = true;
+	pool.getConnection(function(err, con) {
+		if (err) throw err;
+		var sql = "INSERT INTO users (email, password) VALUES " + response;
+		con.query("SELECT * FROM users WHERE email = '" + email + "'", function(err, result) {
+			if (err) throw err;
+			if (result.length != 0) {
+
+				if (result[0].password != pass) {
+					console.log("You inputed the wrong password.");
+				} else {
+					finEmail = email;
+					res.redirect("chat.html");
+					/*
+					This has to be moved!!
+					*/
+				}
+			} else {
+				console.log("You have not created an account");
+			}
+		});
+		con.release();
+	});
+
+
+
 })
 
 var msgs = []
 
 
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    
-    console.log("Example app listening at http://%s:%s", host, port)
- })
- 
- var io = require('socket.io').listen(server); 
- io.on("connection", function(socket) {
-    socket.on('username', function(username) {
-        const keys = Object.keys(users); 
-        if(keys.includes(finEmail)){
-            socket.emit("redirect"); 
-            return; 
-        }
-        users[finEmail] = socket;  
-        
-        
-        socket.username = finEmail;
-        socket.emit('set_name', finEmail); 
-        
-        pool.getConnection(function(err, con) {
-            if (err) throw err;
-        
-            con.query("SELECT email FROM users", function (err, result, fields) {
-                if (err) throw err;
-                socket.emit('data', result, socket.username); 
-               
-            });
-            con.release(); 
-        }); 
-        
-    });
+var server = app.listen(8081, function() {
+	var host = server.address().address
+	var port = server.address().port
 
-    socket.on('disconnect', function() {
-        if(!(socket.partner in users)){
-            delete users[socket.username];
-            return; 
-        } 
-        users[socket.partner].emit('left', socket.username); 
-        delete users[socket.username];    
-    });
+	console.log("Example app listening at http://%s:%s", host, port)
+})
 
-    socket.on('chat_message', function(message) {
-        //io.emit('chat_message', message, socket.username);
-        
-        if(socket.partner == ""){
-            socket.emit('no_partner'); 
-            return;
-        }
-        if(users[socket.partner] != null && users[socket.partner].partner == socket.username) {
-            users[socket.partner].emit('chat_message', message, socket.username, 0); 
-        }
-       // users[socket.username].emit('chat_message', message, socket.username, 1);
-        ///socket.msgs.push([message, new Date(Date.now())]); 
-        processMsgs(socket.username, message, socket.partner); 
-    });
+var io = require('socket.io').listen(server);
+io.on("connection", function(socket) {
+	socket.on('username', function(username) {
+		const keys = Object.keys(users);
+		if (keys.includes(finEmail)) {
+			socket.emit("redirect");
+			return;
+		}
+		users[finEmail] = socket;
 
-    socket.on('found_part', function(username){
-        users[socket.username].emit('clear'); 
-        socket.partner = username; 
-        
-        pool.getConnection(function(err, con) {
-            if (err) throw err;
-           
-            var firstData = [];
-            con.query("SELECT data, dt FROM " + socket.username +" WHERE name ='" + socket.partner+"'", function(err, result){
-                if(err) throw err; 
-                firstData = result;
-                
-                var secondData = [];
-                //🔵
 
-                con.query("SELECT data, dt FROM " + socket.partner +" WHERE name ='" + socket.username+"'", function(err, result){
-                    if(err) throw err; 
-                    secondData = result;                    
-                    socket.emit('is_online', username, finEmail, firstData, secondData);
-                    socket.emit('update_time');
-                    if(socket.partner in users && users[socket.partner].partner == socket.username) users[socket.partner].emit("joined", socket.username); 
-                    
-                });
-               
-            });
-            con.release(); 
-        }); 
-        
-        
-    });
+		socket.username = finEmail;
+		socket.emit('set_name', finEmail);
 
-    
-   
+		pool.getConnection(function(err, con) {
+			if (err) throw err;
 
- });
+			con.query("SELECT email FROM users", function(err, result, fields) {
+				if (err) throw err;
+				socket.emit('data', result, socket.username);
 
- function processMsgs(username, msgs, partner){
-    pool.getConnection(function(err, con) {
-        if(err) throw err; 
-       
-            msgs = msgs.replace(/'/g, "''");
-            msgs = msgs.replace(/"/g,'""' );
-            var sql = "INSERT INTO " + username + " ( name, data) VALUES ('" + partner + "', '" + msgs + "')";
-            
-            con.query(sql, function(err, result){
-                if (err) throw err; 
-            });
-        
-        con.release(); 
-    });
- }
- 
+			});
+			con.release();
+		});
+
+	});
+
+	socket.on('disconnect', function() {
+		if (!(socket.partner in users)) {
+			delete users[socket.username];
+			return;
+		}
+		users[socket.partner].emit('left', socket.username);
+		delete users[socket.username];
+	});
+
+	socket.on('chat_message', function(message) {
+		//io.emit('chat_message', message, socket.username);
+
+		if (socket.partner == "") {
+			socket.emit('no_partner');
+			return;
+		}
+		if (users[socket.partner] != null && users[socket.partner].partner == socket.username) {
+			users[socket.partner].emit('chat_message', message, socket.username, 0);
+		}
+		// users[socket.username].emit('chat_message', message, socket.username, 1);
+		///socket.msgs.push([message, new Date(Date.now())]); 
+		processMsgs(socket.username, message, socket.partner);
+	});
+
+	socket.on('found_part', function(username) {
+		users[socket.username].emit('clear');
+		socket.partner = username;
+
+		pool.getConnection(function(err, con) {
+			if (err) throw err;
+
+			var firstData = [];
+			con.query("SELECT data, dt FROM " + socket.username + " WHERE name ='" + socket.partner + "'", function(err, result) {
+				if (err) throw err;
+				firstData = result;
+
+				var secondData = [];
+				//🔵
+
+				con.query("SELECT data, dt FROM " + socket.partner + " WHERE name ='" + socket.username + "'", function(err, result) {
+					if (err) throw err;
+					secondData = result;
+					socket.emit('is_online', username, finEmail, firstData, secondData);
+					socket.emit('update_time');
+					if (socket.partner in users && users[socket.partner].partner == socket.username) users[socket.partner].emit("joined", socket.username);
+
+				});
+
+			});
+			con.release();
+		});
+
+
+	});
+
+
+
+
+});
+
+function processMsgs(username, msgs, partner) {
+	pool.getConnection(function(err, con) {
+		if (err) throw err;
+
+		msgs = msgs.replace(/'/g, "''");
+		msgs = msgs.replace(/"/g, '""');
+		var sql = "INSERT INTO " + username + " ( name, data) VALUES ('" + partner + "', '" + msgs + "')";
+
+		con.query(sql, function(err, result) {
+			if (err) throw err;
+		});
+
+		con.release();
+	});
+}
